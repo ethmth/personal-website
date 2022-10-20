8131c0a7627d47a9f936fe93bb5badd39dd4f943
ethmth <ethan@smaze.net>
2022-10-19 23:31:20 -0400

    added dark switch

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,26 +1,48 @@
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  DarkMode,
+  Flex,
+  Heading,
+  Text,
+  useColorMode,
+  Switch,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 const HomePage: React.FC = () => {
+  const { toggleColorMode } = useColorMode();
   const [counter, setCounter] = useState(0);
 
-  const MyFunc = () => {
-    console.log("rendered");
+  // Executes on first render, the second argument list
+  // contains elements that cause useEffect to run when they change
+  //useEffect(() => console.log("first render"), []);
+
+  // Enables darkmode by default
+  useEffect(() => toggleColorMode(), []);
+
+  const [darkSwitchChecked, setDarkSwitchChecked] = useState(true);
+
+  const darkModeToggled = () => {
+    setDarkSwitchChecked((darkSwitchChecked) => !darkSwitchChecked);
+    toggleColorMode();
   };
 
-  useEffect(() => console.log("first"), []);
-
-  const epicFunction = () => {
+  const buttonPressed = () => {
     setCounter((counter) => counter + 1);
   };
 
   return (
-    <div>
-      <h1>Ethan Thomas</h1>
-      <p>Hi my name is Ethan. 20M looking for hot signles in the area.</p>
-      <h2>{counter}</h2>
-      <Button onClick={epicFunction}>Push Me</Button>
-    </div>
+    <>
+      <Heading as="h1">Ethan Thomas</Heading>
+      <Text>
+        Hi my name is Ethan. Something really cool will go here some day.
+      </Text>
+      <Heading as="h6">{counter}</Heading>
+      <Button onClick={buttonPressed}>Push Me</Button>
+      <Switch isChecked={darkSwitchChecked} onChange={darkModeToggled}>
+        Dark
+      </Switch>
+    </>
   );
 };
 
