46e641e2f88c84937024c86712672aef44fcaacd
ethmth <ethan@smaze.net>
2022-10-22 20:04:52 -0400

    chakra setup

---
+++ pages/JeffPage.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const JeffPage: NextPage = () => {
+  return <div>This is jeff</div>;
+};
+
+export default JeffPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,25 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import HomePage from "../src/components/HomePage";
+//import HomePage from "../src/components/HomePage";
+import Header from "../src/components/Header";
+import {
+  Button,
+  DarkMode,
+  Flex,
+  Heading,
+  Text,
+  useColorMode,
+  Switch,
+} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
-  return <HomePage />;
+  return (
+    <>
+      <Header />
+      <Heading> Hi, my name is Ethan </Heading>
+    </>
+  );
 };
 
 export default Home;
