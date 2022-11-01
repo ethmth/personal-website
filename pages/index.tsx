70c2e0ea475938ea5814a818d0351c40e5a17872
ethmth <ethan@smaze.net>
2022-11-01 00:15:30 -0400

    added image

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 import { useColorMode, Flex, Heading, Stack, Image } from "@chakra-ui/react";
+// import { Image } from "@chakra-ui/image";
 
 import Container from "../src/components/Container";
 
 import Resume from "../src/components/Resume";
+
+// import Profile from "../src/images/Ethan.jpg";
 
 const Home: NextPage = () => {
   const { colorMode } = useColorMode();
@@ -36,7 +39,9 @@ const Home: NextPage = () => {
             alignItems="flex-start"
             maxWidth="850px"
           >
-            <Image src="../src/images/Ethan.jpg" alt="A Picture of Me" />
+            {/* <Image src="../src/images/Ethan" alt="A Picture of Me" /> */}
+            <Image src="/images/Ethan.jpg" alt="A Picture of Me" />
+            {/* <Image src="https://bit.ly/dan-abramov" alt="A Picture of Me" /> */}
             <Heading as="h1" size="4xl" mb={4}>
               Hi, I am Ethan Thomas
             </Heading>
