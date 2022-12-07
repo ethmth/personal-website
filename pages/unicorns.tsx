3bf6f2ac1b471a271b6a1054c1b84c59463867ec
ethmth <ethan@smaze.net>
2022-12-07 00:55:25 -0500

    Go with sockets.io

--- pages/unicorns.tsx
+++ pages/unicorns.tsx
@@ -10,72 +10,20 @@ import { resolve } from "path";
 
 import io from "socket.io-client";
 
-// const API_URL: string = "http://localhost:5000/";
+const API_URL: string = "ws://localhost:5000/echo";
 // const WebSocket = require("ws");
 
 export const isBrowser = typeof window !== "undefined";
 
 const Unicorns: NextPage = () => {
-  const [words, setWords] = useState<string>("");
-
-  // const isBrowser = typeof window !== "undefined";
-  // const [ws] = useState(() => isBrowser ? new WebSocket("ws://" + API_URL + "echo") : null);
-
-  // ws.onopen = (event: MessageEvent) => {
-  //   ws.send("Hello jeff");
-  // };
-
-  // ws.onmessage = function (event: MessageEvent) {
-  //   const data = event.data;
-
-  //   setWords(words + data + "\n");
-  // };
-
   const [wsInstance, setWsInstance] = useState<any>(null);
 
-  /*
-  // Call when updating the ws connection
-  const updateWs = useCallback((url: string) => {
-     if(!isBrowser) return setWsInstance(null);
-     
-     // Close the old connection
-     if(wsInstance != null && wsInstance?.readyState !== 3) {
-       wsInstance.close();
-     }
-  
-     // Create a new connection
-     const newWs = new WebSocket(url);
-     setWsInstance(newWs);
-  }, [wsInstance])
-  */
-
-  // (Optional) Open a connection on mount
-  // useEffect(() => {
-  //   if(isBrowser) {
-  //     const ws = new WebSocket(API_URL);
-  //     setWsInstance(ws);
-  //   }
-
-  //   return () => {
-  //    // Cleanup on unmount if ws wasn't closed already
-  //    if(ws?.readyState !== 3)
-  //     ws.close()
-  //   }
-  //  }, [])
-
-  useEffect(() => {
-    console.log("hello");
-
-    const socket = io("ws://localhost:5000/echo");
-    socket.on("now", (data) => {
-      setWords(data.message);
-    });
-  }, []);
+  // GO WITH SOCKETS.IO
 
   return (
     <>
       <Text>Hello World</Text>
-      <Text>{words}</Text>
+      {/* <Text>{words}</Text> */}
     </>
   );
 };
