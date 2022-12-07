dcce4cc3a2092c3c25f07e810f2b2a426c9a2cb7
ethmth <ethan@smaze.net>
2022-12-06 20:23:31 -0500

    played around with returning JSX elements

--- pages/unicorns.tsx
+++ pages/unicorns.tsx
@@ -5,29 +5,15 @@ import { useColorMode, Text, Stack } from "@chakra-ui/react";
 import ScoreSquare from "../src/components/unicorns/ScoreSquare";
 import { setRevalidateHeaders } from "next/dist/server/send-payload";
 import Resume from "../src/components/Resume";
+import { Resolver } from "dns";
+import { resolve } from "path";
 
 const API_URL = "http://localhost:5000/";
 
+const [mysquares, setMysqaures] = useState<JSX.Element[]>([]);
+
 const Unicorns: NextPage = () => {
-  // const myResolve = async (result: Promise<any>, name: string) => {
-  //   // const ret: string = "3";
-  //   // const [ret, setRet] = useState("2");
-
-  //   // let ret: string = "2";
-
-  //   let ret = await new Promise<any>((resolve) => {
-  //     resolve(result);
-  //   }).then((r) => console.log(r[name]));
-
-  //   return ret;
-  //   // return "bruh";
-  // };
-
-  // const [on, setOn] = useState(false);
-
-  // useEffect(() => {
-  // getScores();
-  // }, []);
+  let scores = {};
 
   const getScores = async () => {
     try {
@@ -44,31 +30,6 @@ const Unicorns: NextPage = () => {
 
       const result = await response.json();
 
-      // var o = JSON.parse(j, (k, v) => (v.map ? v[0] : v.$ || v));
-      // console.log(o);
-
-      // const scoreCopy = scores;
-
-      // for (let score in result) {
-      // console.log(score);
-      // () => setScores({ ...scores, [score]: result[score] });
-      // setScores({ ...scores, [score]: result[score] });
-      // }
-
-      // return result();
-
-      // setScores({ ...scores, ["red_score"]: result["red_score"] });
-
-      // let blue = result["blue_score"];
-      // let blue = result[0];
-      // console.log(blue);
-      // for( score: result) {
-
-      // }
-
-      // console.log("result is: ", JSON.stringify(result, null, 4));
-      // setScores(JSON.stringify(result, null, 4));
-      // console.log(scores);
       return result;
     } catch {
       return "Error!";
@@ -76,36 +37,46 @@ const Unicorns: NextPage = () => {
   };
 
   const updateScores = async () => {
+    let scoreSquares: JSX.Element[] = [];
+
     const result = await getScores();
     console.log(result);
 
-    let myinter = {};
-
     for (let score in result) {
-      // console.log(score);
-      // () => setScores({ ...scores, [score]: result[score] });
-      // setScores((scores) => ({ ...scores, [score]: result[score] }));
-      myinter = { ...myinter, [score]: result[score] };
+      scores = { ...scores, [score]: result[score] };
+      scoreSquares.push(<ScoreSquare team="test" score="1" />);
     }
-    console.log(myinter);
-    // setScores(myinter);
+    console.log(scores);
+    console.log(scoreSquares);
+
+    // return <div>{scoreSquares[0]}</div>;
+
+    setMysqaures(scoreSquares);
   };
 
-  const [scores, setScores] = useState({});
-  // getScores();
   updateScores();
-
-  // setOn(true);
 
   return (
     <>
       <Text>Hello World</Text>
-      <ScoreSquare team="red" score="1" />
+
+      {mysquares}
+
+      {/* {updateScores().map((scoreSquare) => ( */}
+      {/* <div>{scoreSquare}</div> */}
+      {/* ))} */}
+
+      {/* {resolve(updateScores())} */}
+
+      {/* {updateScores().map ? : } */}
+
+      {/* {for(let team in scores) { */}
+
+      {/* // }} */}
+      {/* <ScoreSquare team="bruh" score="1" /> */}
+      {/* {updateScores()} */}
 
       {/* <Text>Scores {scores}</Text> */}
-      {/* {scores} */}
-
-      {/* <button onClick={() => setOn(true)}>Click me</button> */}
     </>
   );
 };
