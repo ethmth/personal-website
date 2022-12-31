90080db0f5fe84e1630d9da57584dd9a037a47d0
ethmth <ethan@smaze.net>
2022-12-31 16:23:25 -0500

    Added socials

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,12 +1,40 @@
-import { Image, Heading, Flex, Text, useColorMode } from "@chakra-ui/react";
+// import { Image, Heading, Flex, Text, useColorMode } from "@chakra-ui/react";
+// import styled from "@emotion/styled";
+// import { bgColor, color } from "../styles/colors";
+// import { useState, useEffect } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "@emotion/styled";
-import { bgColor, color } from "../styles/colors";
-import { useState, useEffect } from "react";
+
+import {
+  faDocker,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Socials: React.FC = () => {
-  const { colorMode } = useColorMode();
+  //   const { colorMode } = useColorMode();
 
-  return <Text>Hello World</Text>;
+  const iconSize = "3x";
+
+  const IconLink = styled.a`
+    margin-left: 1em;
+    margin-right: 1em;
+  `;
+
+  return (
+    <div>
+      <IconLink href="https://github.com/ethmth" target="_blank">
+        <FontAwesomeIcon icon={faGithub} size={iconSize} />
+      </IconLink>
+      <IconLink href="https://www.linkedin.com/in/ethanmt/" target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} size={iconSize} />
+      </IconLink>
+      <IconLink href="https://hub.docker.com/u/ethmth" target="_blank">
+        <FontAwesomeIcon icon={faDocker} size={iconSize} />
+      </IconLink>
+    </div>
+  );
 };
 
 export default Socials;
