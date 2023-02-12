afd630329fe6d988eb6a7bff49be86678c21e5f1
ethmth <ethan@smaze.net>
2023-02-11 19:28:45 -0500

    Fixed Video Page Tags for Desktop; Mobile Still Doesn't Look Good

--- src/components/VideoLink.tsx
+++ src/components/VideoLink.tsx
@@ -1,7 +1,7 @@
 import { Heading, Text, Image, useColorMode } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import styled from "@emotion/styled";
-import { bgColor } from "../styles/colors";
+import { bgColor, conColor } from "../styles/colors";
 
 interface VideoLinkProps {
   title: string;
@@ -15,12 +15,11 @@ const VideoLink: React.FC<VideoLinkProps> = (props) => {
   const { colorMode } = useColorMode();
 
   const TagsDiv = styled.div`
-    maxwidth: 800px;
-    width: 100%;
+    margin-left: 1em;
   `;
 
   const TagsSpan = styled.span`
-    background-color: ${bgColor[colorMode]};
+    background-color: ${conColor[colorMode]};
     border-radius: 50px;
     padding: 0.5em 1em 0.5em 1em;
     margin: 0 1em 0 0;
@@ -38,15 +37,19 @@ const VideoLink: React.FC<VideoLinkProps> = (props) => {
 
   return (
     <>
-      <TagsDiv>
-        {props.tags?.map((tag) => (
-          <TagsSpan>{tag}</TagsSpan>
-        ))}
-      </TagsDiv>
       <StyledLink href={props.href}>
-        <Image src={props.thumbnail} height="150px" borderRadius="10px" />
+        <Image src={props.thumbnail} height="10em" borderRadius="10px" />
         <div>
-          <Heading ml="20px">{props.title}</Heading>
+          <TagsDiv>
+            {props.tags?.map((tag) => (
+              <TagsSpan>{tag}</TagsSpan>
+            ))}
+          </TagsDiv>
+
+          <br />
+          <Heading mt="1em" ml="20px">
+            {props.title}
+          </Heading>
           <Text ml="20px" mt="1em">
             {props.children}
           </Text>
