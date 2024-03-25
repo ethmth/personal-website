8b01f2a6ef5289935c0008ee42a135d05a3c8db9
ethmth <ethan@smaze.net>
2024-03-25 14:13:38 -0400

    PDF displays in Page but text below also displays; need to remove the bottom text, fix the width/height, and add download button

--- pages/resume_pdf.tsx
+++ pages/resume_pdf.tsx
@@ -1,13 +1,9 @@
-import NoSsr from "../src/components/NoSsr";
-
 import type { NextPage } from "next";
 import PDFDisplay from "../src/components/PDFDisplay";
 
 const ResumePDFPage: NextPage = () => {
   return (
-    <NoSsr>
-      <PDFDisplay file="https://raw.githubusercontent.com/ethmth/resume/main/EthanThomasResume.pdf" />
-    </NoSsr>
+    <PDFDisplay file="https://raw.githubusercontent.com/ethmth/resume/main/EthanThomasResume.pdf" />
   );
 };
 
--- src/components/PDFDisplay.tsx
+++ src/components/PDFDisplay.tsx
@@ -1,19 +1,62 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 import NoSsr from "./NoSsr";
-import { Document } from "react-pdf";
-import { pdfjs } from 'react-pdf';
+import { Document, Page } from "react-pdf";
+
+import styled from "@emotion/styled";
+import { pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const DocContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const options = {
+  cMapUrl: "/cmaps/",
+  standardFontDataUrl: "/standard_fonts/",
+};
 
 interface PDFDisplayProps {
   file: string;
 }
 
 const PDFDisplay: React.FC<PDFDisplayProps> = (props) => {
+  const [widthPDF, setWidthPDF] = useState(1000);
+
+  function updateWidth() {
+    setWidthPDF(window.innerWidth * 0.8);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+
+    updateWidth();
+  }, []);
+
   return (
     <NoSsr>
-      <Document file={props.file}></Document>
+      <Container>
+        <DocContainer>
+          <Document file={props.file} options={options}>
+            <Page key={1} pageNumber={1} width={widthPDF}></Page>
+          </Document>
+        </DocContainer>
+      </Container>
     </NoSsr>
   );
 };
