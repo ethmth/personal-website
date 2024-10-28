4242f5d004a479da979e94e3b1193f1d50d7ad00
ethmth <ethan@smaze.net>
2024-10-28 15:29:28 -0400

    Updated dependencies and setup reactive resume redirect

--- next.config.js
+++ next.config.js
@@ -2,16 +2,21 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/resume_pdf",
-  //       destination:
-  //         "https://docs.google.com/gview?url=https://raw.githubusercontent.com/ethmth/resume/main/EthanThomasResume.pdf&embedded=true",
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      // {
+      //   source: "/resume_pdf",
+      //   destination:
+      //     "https://docs.google.com/gview?url=https://raw.githubusercontent.com/ethmth/resume/main/EthanThomasResume.pdf&embedded=true",
+      //   permanent: false,
+      // },
+      {
+        source: "/reactive_resume",
+        destination: "https://rxresu.me/rrethan/ethan-thomas",
+        permanent: false,
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
 
